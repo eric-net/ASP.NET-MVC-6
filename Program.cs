6341136e5652391eb7af0ad5df5ebaa824c350e5
Eric Samuel Barrionuevo <ericnetcore@gmail.com>
2024-09-14 17:20:42 -0300

    subida proyecto asp net

---
+++ Program.cs
@@ -0,0 +1,37 @@
+using DemoAPI.Models.Data;
+using DemoAPI.Models.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IProductRepository, ProductRepository>();
+
+builder.Services.AddDbContext<DemoContext>(
+    options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+
+    });
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
